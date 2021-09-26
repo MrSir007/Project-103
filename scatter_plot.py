1e8809f7c787e44d574a1223d74994e3aa117a3f
MrSir <73583589+MrSir007@users.noreply.github.com>
2021-09-26 20:04:27 +1000

    Update scatter_plot.py

--- scatter_plot.py
+++ scatter_plot.py
@@ -4,5 +4,5 @@ import plotly.express as px
 import csv
 
 df = pd.read_csv("data.csv")
-sp = px.scatter(df, x="date", y="cases", color="country", size="cases", size_max=20, title="Scatter Plot")
+sp = px.scatter(df, x="date", y="cases", color="country", size_max=20, title="Scatter Plot")
 sp.show()
